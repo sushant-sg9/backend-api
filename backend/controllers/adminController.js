deefb64d7477966d159f055c5e0e6748dc54c1aa
sushant-sg9 <sushantghadge2016@gmail.com>
2024-10-14 12:37:39 +0530

    api created for popularity

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -50,4 +50,67 @@ const getAllUsers = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { adminLogin, getAllUsers };
+
+const getPopularity = asyncHandler(async (req, res) => {
+    try {
+        const users = await User.find({}, 'videoLinks');
+
+        const allLinks = users.flatMap(user => 
+            user.videoLinks.map(video => ({
+                link: video.link,
+                userId: user._id,
+                userCount: video.count 
+            }))
+        );
+
+        const linkCountMap = allLinks.reduce((acc, { link, userId, userCount }) => {
+            if (!acc[link]) {
+                acc[link] = { count: 0, users: [] };
+            }
+            acc[link].count += 1;
+            acc[link].users.push({ userId, count: userCount });
+            return acc;
+        }, {});
+
+        const sortedLinks = Object.entries(linkCountMap)
+            .map(([link, { count, users }]) => ({
+                link,
+                count,
+                users: users.sort((a, b) => b.count - a.count)  
+            }))
+            .sort((a, b) => b.count - a.count);  
+
+        res.status(200).json({
+            success: true,
+            message: "Video links retrieved successfully by popularity",
+            data: sortedLinks,
+        });
+    } catch (error) {
+        res.status(500);
+        throw new Error('Failed to retrieve video links by popularity');
+    }
+});
+
+const getUserById = asyncHandler(async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await User.findById(id);
+
+        if (!user) {
+            res.status(404);
+            throw new Error('User not found');
+        }
+
+        res.status(200).json({
+            success: true,
+            message: 'User data retrieved successfully',
+            data: user,
+        });
+    } catch (error) {
+        res.status(500);
+        throw new Error('Failed to retrieve user data');
+    }
+});
+
+module.exports = { adminLogin, getAllUsers, getPopularity, getUserById };
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { adminLogin, getAllUsers } = require('../controllers/adminController');
+const { adminLogin, getAllUsers, getPopularity, getUserById } = require('../controllers/adminController');
 const  {adminProtect}  = require('../middleware/authController')
 
 const router = express.Router();
 
 router.route('/login').post(adminLogin);
 router.route('/getAllUser').get(adminProtect,getAllUsers);
+router.route('/getPopularity').get(adminProtect,getPopularity);
+router.route('/getUserById/:id').get(adminProtect, getUserById);
+
 
 module.exports = router;
+
