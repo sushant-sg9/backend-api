dece894d7f153c40d405d34d0281153d87c385d8
sushant-sg9 <sushantghadge2016@gmail.com>
2024-10-15 13:14:37 +0530

    api created for email otp

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -484,14 +484,13 @@ const getVideoLinkDetails = asyncHandler(async (req, res) => {
     }
 
 });
-
 const transporter = nodemailer.createTransport({
-    host: 'smtp.hostinger.com',  
-    port: 465,  
-    secure: true,  
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true, 
     auth: {
-        user: 'donotreply@hookstep.net', 
-        pass: 'Saasinsider@16', 
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
     },
     debug: true,
 });
