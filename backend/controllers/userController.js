6a7eb75c17fb56537ffaa3015eca5010c826e9cb
Sushant <sushantghadge2016@gmail.com>
2025-01-17 15:21:43 +0530

    email otp

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -128,7 +128,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (userExists) {
         res.status(400);
-        throw new Error("User already exists");
+        throw new Error("Mobile Number already exists");
     }
 
 
