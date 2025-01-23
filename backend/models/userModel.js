229fdc1718c53f67658ad8a430c3853f22f4a62c
Sushant <sushantghadge2016@gmail.com>
2025-01-23 12:43:37 +0530

    new login api

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -22,6 +22,9 @@ const userSchema = mongoose.Schema({
     password: {
         type: String
     },
+    occupation:{
+            type: String
+    },
     country: {
         type: String
     },
