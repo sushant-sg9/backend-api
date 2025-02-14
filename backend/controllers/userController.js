fb7bfe132b521aa5ca7ce1fc67516a64645ca9fb
Sushant <sushantghadge2016@gmail.com>
2025-02-14 19:15:59 +0530

    billing and invoice

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,7 +66,7 @@ const newSignup = asyncHandler(async (req, res) => {
         `;
 
         const mailOptions = {
-            from: '"HookStep" <donotreply@hookstep.net >',
+            from: '"HookStep" <donotreply@hookstep.net>',
             to: email,
             subject: "Welcome to HookStep - Verify Your Email",
             text: `Your OTP for verification is: ${otp}`,
