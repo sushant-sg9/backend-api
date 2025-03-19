bd22b1bbb83c380148fee89254981d6a7996ed3b
GaziWani1 <gaziwani123@gmail.com.com>
2025-03-19 17:12:32 +0530

    brevo setup email

--- backend/Config/brevomail.config.js
+++ backend/Config/brevomail.config.js
@@ -1,7 +1,7 @@
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.BREVO_API_KEY;
+apiKey.apiKey = process.env.BREVO_API;
 const ApiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
